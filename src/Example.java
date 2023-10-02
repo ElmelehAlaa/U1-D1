cbb6cc5c172d497f580233aba054c40a066b61a6
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-10-02 16:28:27 +0200

    Scanner

--- src/Example.java
+++ src/Example.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
@@ -24,7 +25,29 @@ public class Example {
     public static void concatenazione(String string , int n3){
         System.out.println(string + " " + n3 + " "+ "anni");
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("inserisci il nome del tuo cane");
+        String name =   input.nextLine();
+        System.out.println("hai inserito" + " "+ name);
+
+
+        System.out.println("Inserisci La Razza del tuo cane");
+        String razza= input.nextLine();
+        System.out.println("hai inserito" + " " + razza);
+
+        System.out.println("Inserisci taglia del tuo cane");
+        String taglia = input.nextLine();
+        System.out.println("hai inserito" + " " + taglia);
+
+        System.out.println(taglia+ razza + name);
+
+
+
+
+        input.close();
     }
+
 
 
 
