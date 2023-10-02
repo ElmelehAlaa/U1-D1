8cb0f272a55ced046f5d25eb2f72cf44765d8965
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-10-02 15:50:50 +0200

    moltiplicazione , concatenazione, array

---
+++ src/Example.java
@@ -0,0 +1,25 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        moltiplicazione(6,5);
+        concatenazione("ciao sono Alaa ed ho", 24  );
+
+        String nomi [] = {"Luca", "Mario", "Giuseppe", "Andrea", "Fabio"};
+
+
+    }
+
+    public static void  moltiplicazione(int n1,int n2){
+
+        System.out.println("il risultato è :"+ (n1 * n2));
+
+    }
+    public static void concatenazione(String string , int n3){
+        System.out.println(string + " " + n3 + " "+ "anni");
+
+    }
+
+
+
+}
